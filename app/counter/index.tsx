c47c22fc411bb70faee48f3f398ab68cf9eac5e5
anthonykijoy <antoni.kijjaroentorn@gmail.com>
2025-03-30 14:44:35 +0200

    feat(counter): add notification scheduling functionality
    Enhance the counter screen by adding the ability to schedule a notification when the user grants permission. If permission is not granted, an alert is shown to prompt the user to enable notifications in settings.

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,21 +1,37 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import * as Notifications from "expo-notifications";
 
 export default function CounterScreen() {
-  const handleRequestPermission = async () => {
+  const scheduleNotifiation = async () => {
     // Request permission logic goes here
     const result = await registerForPushNotificationsAsync();
-    console.log(result); // Log the result for dem
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm your notifiation from the app",
+        },
+        trigger: {
+          type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+          seconds: 5,
+        },
+      });
+    } else {
+      Alert.alert(
+        "Unable to schedule notification",
+        "Enable the notifications permission for Expo Go in settings",
+      );
+    }
   };
   return (
     <View style={styles.container}>
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.8}
-        onPress={handleRequestPermission}
+        onPress={scheduleNotifiation}
       >
-        <Text style={styles.buttonText}>Request Permission</Text>
+        <Text style={styles.buttonText}>Schedule Notification</Text>
       </TouchableOpacity>
     </View>
   );
