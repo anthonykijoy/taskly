0099489e753f9299745d564a0cc3ef9af84c27fb
anthonykijoy <antoni.kijjaroentorn@gmail.com>
2025-03-30 16:36:59 +0200

    feat(counter): persist countdown state and notification ID
    Add functionality to persist the countdown state and notification ID using local storage. This ensures that the countdown and notification data are retained across app sessions. Additionally, update the notification scheduling logic to use the persisted state for better consistency.

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -5,9 +5,17 @@ import * as Notifications from "expo-notifications";
 import { useState, useEffect } from "react";
 import { intervalToDuration, isBefore } from "date-fns";
 import { TimeSegment } from "../../components/TimeSegment";
+import { getFromStorage, saveToStorage } from "../../utils/storage";
 
 // 10 seconds from now
-const timestamp = Date.now() + 10 * 1000;
+const frequency = 10 * 1000;
+
+const countdownStorageKey = "taskly-countdown";
+
+type PersistedCountdownState = {
+  currentNotificationId: string | undefined;
+  completedAtTimestamp: number[];
+};
 
 type CountdownStatus = {
   isOverdue: boolean;
@@ -15,15 +23,30 @@ type CountdownStatus = {
 };
 
 export default function CounterScreen() {
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountdownState>();
   const [status, setStatus] = useState<CountdownStatus>({
     isOverdue: false,
     distance: {},
   });
 
-  console.log(status);
+  // console.log(status);
+
+  const lastCompletedTimestamp = countdownState?.completedAtTimestamp[0];
+
+  useEffect(() => {
+    const init = async () => {
+      const value = await getFromStorage(countdownStorageKey);
+      setCountdownState(value);
+    };
+    init();
+  }, []);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
+      const timestamp = lastCompletedTimestamp
+        ? lastCompletedTimestamp + frequency
+        : Date.now();
       const isOverdue = isBefore(timestamp, Date.now());
       const distance = intervalToDuration(
         isOverdue
@@ -39,19 +62,20 @@ export default function CounterScreen() {
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, [lastCompletedTimestamp]);
 
   const scheduleNotifiation = async () => {
+    let pushNotificationId;
     // Request permission logic goes here
     const result = await registerForPushNotificationsAsync();
     if (result === "granted") {
-      await Notifications.scheduleNotificationAsync({
+      pushNotificationId = await Notifications.scheduleNotificationAsync({
         content: {
-          title: "I'm your notifiation from the app",
+          title: "The thing is due!",
         },
         trigger: {
           type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
-          seconds: 5,
+          seconds: frequency / 1000,
         },
       });
     } else {
@@ -61,6 +85,20 @@ export default function CounterScreen() {
         "Enable the notifications permission for Expo Go in settings"
       );
     }
+    if (countdownState?.currentNotificationId) {
+      await Notifications.cancelScheduledNotificationAsync(
+        // eslint-disable-next-line
+        countdownState?.currentNotificationId
+      );
+    }
+    const newCountdownState: PersistedCountdownState = {
+      currentNotificationId: pushNotificationId,
+      completedAtTimestamp: countdownState
+        ? [Date.now(), ...countdownState.completedAtTimestamp, Date.now()]
+        : [Date.now()],
+    };
+    setCountdownState(newCountdownState);
+    await saveToStorage(countdownStorageKey, newCountdownState);
   };
   return (
     <View
