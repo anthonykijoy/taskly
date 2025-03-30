2d3a08bcc8c804b26fd2d98c4000be4f7450027f
anthonykijoy <antoni.kijjaroentorn@gmail.com>
2025-03-30 16:07:14 +0200

    feat(counter): enhance UI with overdue status and time segments
    Add TimeSegment component to display days, hours, minutes, and seconds. Update container style to reflect overdue status and modify button text for better clarity. These changes improve user experience by providing clearer visual feedback and time tracking.

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -4,6 +4,7 @@ import { registerForPushNotificationsAsync } from "../../utils/registerForPushNo
 import * as Notifications from "expo-notifications";
 import { useState, useEffect } from "react";
 import { intervalToDuration, isBefore } from "date-fns";
+import { TimeSegment } from "../../components/TimeSegment";
 
 // 10 seconds from now
 const timestamp = Date.now() + 10 * 1000;
@@ -62,13 +63,45 @@ export default function CounterScreen() {
     }
   };
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        status.isOverdue ? styles.containerLate : undefined,
+      ]}
+    >
+      {status.isOverdue ? (
+        <Text style={(styles.heading, styles.whiteText)}>Thing overdue by</Text>
+      ) : (
+        <Text style={(styles.heading, styles.whiteText)}>Thing due in...</Text>
+      )}
+      <View style={styles.row}>
+        <TimeSegment
+          unit="Days"
+          number={status.distance.days ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Hours"
+          number={status.distance.hours ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Minutes"
+          number={status.distance.minutes ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Seconds"
+          number={status.distance.seconds ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+      </View>
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.8}
         onPress={scheduleNotifiation}
       >
-        <Text style={styles.buttonText}>Schedule Notification</Text>
+        <Text style={styles.buttonText}>I've done the thing!</Text>
       </TouchableOpacity>
     </View>
   );
@@ -81,6 +114,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#fff",
   },
+  containerLate: {
+    backgroundColor: theme.colorRed,
+  },
   button: {
     backgroundColor: theme.colorBlack,
     padding: 10,
@@ -91,5 +127,17 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textTransform: "uppercase",
     letterSpacing: 1,
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 24,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  whiteText: {
+    color: theme.colorWhite,
   },
 });
